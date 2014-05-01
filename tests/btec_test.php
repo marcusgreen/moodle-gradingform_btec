58abb35e1814659886d4144da23b761e20da9b44
marcusgreen <marcusavgreen@gmail.com>
2014-05-01 21:30:49 +0100

    updates to tests

--- tests/btec_test.php
+++ tests/btec_test.php
@@ -21,123 +21,118 @@
  * @copyright  2012 Marcus Green
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-//defined('MOODLE_INTERNAL') || die();
 global $CFG;
 
-require_once($CFG->dirroot .'/grade/grading/form/btec/bteceditor.php');
-require_once($CFG->dirroot .'/grade/grading/form/btec/renderer.php');
-require_once($CFG->dirroot .'/grade/grading/form/btec/lib.php');
+require_once($CFG->dirroot . '/grade/grading/form/btec/bteceditor.php');
+require_once($CFG->dirroot . '/grade/grading/form/btec/renderer.php');
+require_once($CFG->dirroot . '/grade/grading/form/btec/lib.php');
 
-require_once($CFG->dirroot .'/lib/pagelib.php');
+require_once($CFG->dirroot . '/lib/pagelib.php');
 
+class btec_test extends basic_testcase {
 
-class btec_test extends basic_testcase{
+    public function test_created_form() {
 
-
-	public function test_created_form() {
-	    
-          $form =new moodlequickform_bteceditor('testbteceditor', 'elementlabel', null);
-          $type= $form->getElementTemplateType();
-          $this->assertEquals('default',$type);
-
+        $form = new moodlequickform_bteceditor('testbteceditor', 'elementlabel', null);
+        $type = $form->getElementTemplateType();
+        $this->assertEquals('default', $type);
     }
-    
-    public function test_grade_calculation(){
-    
-//define('MIN_VALUE', '0.0');
-    
-//		define("REFER",1);
-	//	define("PASS",2);
-	//	define("MERIT",3);
-	//	define("DISTINCTION",4);
-    
 
-    $criteria=array('criteria'=> array(
-    array('score'=>0,'level'=>'Pass'),
-    array('score'=>0,'level'=>'Pass'),
-    array('score'=>0,'level'=>'Merit'),
-    array('score'=>0,'level'=>'Merit'),
-    array('score'=>0,'level'=>'Distinction'),
-    array('score'=>0,'level'=>'Distinction')
-    ));
-    
-    
-    $controller='';
-    $data='';
-    $form = new gradingform_btec_instance($controller,$data);
-    $levelmet=$form->calculate_btec_grade($criteria);
-    $this->assertEquals(REFER,$levelmet);
-    
-     $criteria=array('criteria'=> array(
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>0,'level'=>'Merit'),
-    array('score'=>0,'level'=>'Merit'),
-    array('score'=>0,'level'=>'Distinction'),
-    array('score'=>0,'level'=>'Distinction')
-    ));
-    
-    $levelmet=$form->calculate_btec_grade($criteria);
-    $this->assertEquals(PASS,$levelmet);
-    
-      
-     $criteria=array('criteria'=> array(
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Merit'),
-    array('score'=>1,'level'=>'Merit'),
-    array('score'=>0,'level'=>'Distinction'),
-    array('score'=>0,'level'=>'Distinction')
-    ));
-    
-    $levelmet=$form->calculate_btec_grade($criteria);
-    $this->assertEquals(MERIT,$levelmet);
-    
-         
-     $criteria=array('criteria'=> array(
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Merit'),
-    array('score'=>1,'level'=>'Merit'),
-    array('score'=>1,'level'=>'Distinction'),
-    array('score'=>1,'level'=>'Distinction')
-    ));
-    
-    $levelmet=$form->calculate_btec_grade($criteria);
-    $this->assertEquals(DISTINCTION,$levelmet);
+    public function test_grade_calculation() {
 
-         
-     $criteria=array('criteria'=> array(
-    array('score'=>0,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Pass'),
-    array('score'=>1,'level'=>'Merit'),
-    array('score'=>1,'level'=>'Merit'),
-    array('score'=>1,'level'=>'Distinction'),
-    array('score'=>1,'level'=>'Distinction')
-    ));
-    
-    $levelmet=$form->calculate_btec_grade($criteria);
-    $this->assertEquals(REFER,$levelmet);
+        /* If you dont get any criteria you get an overal REFER */
+        $criteria = array('criteria' => array(
+                array('score' => 0, 'level' => 'Pass'),
+                array('score' => 0, 'level' => 'Pass'),
+                array('score' => 0, 'level' => 'Merit'),
+                array('score' => 0, 'level' => 'Merit'),
+                array('score' => 0, 'level' => 'Distinction'),
+                array('score' => 0, 'level' => 'Distinction')
+        ));
+        $controller = '';
+        $data = '';
+        $form = new gradingform_btec_instance($controller, $data);
+        $levelmet = $form->calculate_btec_grade($criteria);
+        $this->assertEquals(REFER, $levelmet);
 
-    
+        /* If you only get the P criteria you get an oveall PASS */
+        $criteria = array('criteria' => array(
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 0, 'level' => 'Merit'),
+                array('score' => 0, 'level' => 'Merit'),
+                array('score' => 0, 'level' => 'Distinction'),
+                array('score' => 0, 'level' => 'Distinction')
+        ));
+        $levelmet = $form->calculate_btec_grade($criteria);
+        $this->assertEquals(PASS, $levelmet);
+
+        /* if you get  everything except a Distinction you get a Merit */
+        $criteria = array('criteria' => array(
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Merit'),
+                array('score' => 1, 'level' => 'Merit'),
+                array('score' => 1, 'level' => 'Distinction'),
+                array('score' => 0, 'level' => 'Distinction')
+        ));
+        $levelmet = $form->calculate_btec_grade($criteria);
+        $this->assertEquals(MERIT, $levelmet);
+
+        /* if you get all criteria you get an overall DISTINCTION */
+        $criteria = array('criteria' => array(
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Merit'),
+                array('score' => 1, 'level' => 'Merit'),
+                array('score' => 1, 'level' => 'Distinction'),
+                array('score' => 1, 'level' => 'Distinction')
+        ));
+
+        $levelmet = $form->calculate_btec_grade($criteria);
+        $this->assertEquals(DISTINCTION, $levelmet);
+
+        /* If there are no Merit criteria and you get all available
+         * you get an overall DISTINCTION
+         */
+        $criteria = array('criteria' => array(
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Distinction'),
+                array('score' => 1, 'level' => 'Distinction')
+        ));
+        $levelmet = $form->calculate_btec_grade($criteria);
+        $this->assertEquals(DISTINCTION, $levelmet);
+
+        /* If you get all except one Pass criteria you
+         * get an overall REFER
+         */
+        $criteria = array('criteria' => array(
+                array('score' => 0, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Pass'),
+                array('score' => 1, 'level' => 'Merit'),
+                array('score' => 1, 'level' => 'Merit'),
+                array('score' => 1, 'level' => 'Distinction'),
+                array('score' => 1, 'level' => 'Distinction')
+        ));
+
+        $levelmet = $form->calculate_btec_grade($criteria);
+        $this->assertEquals(REFER, $levelmet);
     }
-    
-    public function test_created_renderer(){
-    
-    $PAGE=new moodle_page();
-    $renderer= new gradingform_btec_renderer($PAGE,1);
-    $options= array();
-    $criterion=1;
-    $value=1;
-    $validationerrors=0;
-    $template = $renderer->criterion_template(1,$options,'btec',$options,$criterion,$validationerrors);
-    $this->assertInternalType('string', $template, 'template returned should be of type string');
-        
+
+    public function test_created_renderer() {
+        $PAGE = new moodle_page();
+        $renderer = new gradingform_btec_renderer($PAGE, 1);
+        $options = array();
+        $criterion = 1;
+        $value = 1;
+        $validationerrors = 0;
+        $template = $renderer->criterion_template(1, $options, 'btec', $options, $criterion, $validationerrors);
+        $this->assertInternalType('string', $template, 'template returned should be of type string');
     }
-    
-    public function test_validate_grading_element(){
-    //setup element value
-      //$this->assertEquals(validate_grading_element($elementvalue);
-    
+
+    public function test_validate_grading_element() {
+        /*setup element value
+        this->assertEquals  (validate_grading_element($elementvalue) */
     }
 }
