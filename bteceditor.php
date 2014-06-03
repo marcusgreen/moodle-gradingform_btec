c1afe332b502441202e5cb0e21aefe619c71755c
marcusgreen <marcusavgreen@gmail.com>
2014-06-03 22:40:10 +0100

    i18n stuff

--- bteceditor.php
+++ bteceditor.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 require_once("HTML/QuickForm/input.php");
-
+require_once(dirname(__FILE__).'/lib.php');
 class moodlequickform_bteceditor extends HTML_QuickForm_input {
 
     /** @var string help message */
@@ -316,16 +316,21 @@ class moodlequickform_bteceditor extends HTML_QuickForm_input {
      * @return string|false error text or false if no errors found
      */
     public function validate($value) {
+        $scaleletters=get_scale_letters();
+        $p=$scaleletters['p'];
+        $m=$scaleletters['m'];
+        $d=$scaleletters['d'];
         $criteria = $value['criteria'];
         $shortnamerror = false;
         $shortnames = array();
         foreach ($criteria as $key => $element) {
             $level = substr($element['shortname'], 0, 1);
             $level = strtolower($level);
-            if ($level != 'p' && $level != 'm' && $level != 'd') {
+            if ($level != $p && $level != $m && $level != $d) {
                 $this->validationerrors.=' '.get_string('startwithpmd', 'gradingform_btec', $element['shortname']);
                 $shortnamerror = true;
             }
+            /* 99 is arbitrary, it should probably be the length of the string */
             $number = substr($element['shortname'], 1, 99);
             if (!is_numeric($number)) {
                 if ($shortnamerror == true) {
